99a2809adbb596ecaaeb044f0cc2ca24811db80c
Trung Duong <trungdt.cmaxx@gmail.com>
2023-02-07 17:07:40 +0700

    sidebar

--- FrontEnd/src/services/user/userService.js
+++ FrontEnd/src/services/user/userService.js
@@ -1,5 +1,18 @@
 import axios from "../../api/axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
-export const getUser = async () => {
-    return axios.get("/users")
-}
+export const getUser = createAsyncThunk(
+  "user/getUser",
+  async (data, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get("/users");
+      return data;
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
+);
